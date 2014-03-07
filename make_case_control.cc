59df215f9f2163926da23864590beacbcfbb514d
Kevin R. Thornton <krthornt@uci.edu>
2014-03-07 14:54:14 -0800

    Some helper functions moved to other files

--- make_case_control.cc
+++ make_case_control.cc
@@ -36,8 +36,8 @@
 
 #include <mutation_with_age.hpp>
 #include <locking_routines.hpp>
+#include <ccintermediate.hpp>
 
-#include <Sequence/PolyTableFunctions.hpp>
 #include <Sequence/SimData.hpp>
 
 #include <iostream>
@@ -60,13 +60,10 @@
 #include <fwdpp/IO.hpp>
 #include <fwdpp/sampling_functions.hpp>
 
+
 using namespace std;
 using namespace Sequence;
 using namespace KTfwd;
-
-typedef gamete_base<TFLmtype> gtype;
-typedef list<TFLmtype> mlist;
-typedef list<gtype> glist;
 
 struct params
 {
@@ -75,31 +72,24 @@ struct params
   double case_proportion;
 };
 
-struct cc_intermediate
-{
-  SimData neutral,causative;
-  vector<char> min_n,min_c; //minor alleles, defined from controls
-  vector<pair<double,double> > phenotypes;
-};
-
 params process_command_line(int argc, char ** argv);
 
-void process_subset( vector< pair<double,string> > & datablock_neut,
-		     vector< pair<double,string> > & datablock_sel,
-		     vector< pair<double,double> > & ccphenos,
-		     const vector< pair<glist::iterator,glist::iterator> > & diploids,
-		     const vector<pair<double,double> > & popphenos,
-		     const vector<size_t> & indlist,
-		     const unsigned & maxnum,
-		     const unsigned & ttl,
-		     const unsigned & offset);
+// void process_subset( vector< pair<double,string> > & datablock_neut,
+// 		     vector< pair<double,string> > & datablock_sel,
+// 		     vector< pair<double,double> > & ccphenos,
+// 		     const vector< pair<glist::iterator,glist::iterator> > & diploids,
+// 		     const vector<pair<double,double> > & popphenos,
+// 		     const vector<size_t> & indlist,
+// 		     const unsigned & maxnum,
+// 		     const unsigned & ttl,
+// 		     const unsigned & offset);
 
-cc_intermediate process_population( const vector< pair<glist::iterator,glist::iterator> > & diploids,
-				    const vector<pair<double,double> > & phenotypes,
-				    const vector<size_t> & put_controls,
-				    const vector<size_t> & put_cases,
-				    const unsigned & ncontrols,
-				    const unsigned & ncases);
+// cc_intermediate process_population( const vector< pair<glist::iterator,glist::iterator> > & diploids,
+// 				    const vector<pair<double,double> > & phenotypes,
+// 				    const vector<size_t> & put_controls,
+// 				    const vector<size_t> & put_cases,
+// 				    const unsigned & ncontrols,
+// 				    const unsigned & ncases);
 
 std::pair<double,double> phenosums(const vector<pair<double,double> > & phenos, const double & case_proportion, double * cutoff);
 
@@ -331,143 +321,4 @@ std::pair<double,double> phenosums(const vector<pair<double,double> > & phenos, 
 			 gsl_stats_sd(&pcopy[0],1,pcopy.size()) );
   //get the upper case_proportion-th'd quantile from the pheno dist
   *cutoff =  gsl_stats_quantile_from_sorted_data(&pcopy[0],1,pcopy.size(),1.-case_proportion);
-}
-
-void process_subset( vector< pair<double,string> > & datablock_neut,
-		     vector< pair<double,string> > & datablock_sel,
-		     vector< pair<double,double> > & ccphenos,
-		     const vector< pair<glist::iterator,glist::iterator> > & diploids,
-		     const vector<pair<double,double> > & popphenos,
-		     const vector<size_t> & indlist,
-		     const unsigned & maxnum,
-		     const unsigned & ttl,
-		     const unsigned & offset)
-{
-  vector< pair<double,string> >::iterator itr;
-
-  for( unsigned i = 0 ; i < maxnum ; ++i )
-    {
-      ccphenos.push_back( popphenos[ indlist[i] ] );
-      //neutral
-      for( unsigned mut = 0 ; mut < diploids[ indlist[i] ].first->mutations.size() ; ++mut )
-	{
-	  double mutpos =  diploids[ indlist[i] ].first->mutations[mut]->pos;
-	  itr = find_if(datablock_neut.begin(),
-			datablock_neut.end(),
-			boost::bind(KTfwd::find_mut_pos(),_1,mutpos));
-	  if( itr == datablock_neut.end() )
-	    {
-	      datablock_neut.push_back( make_pair(mutpos,string('0',ttl)) );
-	      datablock_neut[datablock_neut.size()-1].second[offset + 2*i] = '1';
-	    }
-	  else
-	    {
-	      itr->second[2*i] = '1';
-	    }
-	}
-      for( unsigned mut = 0 ; mut < diploids[ indlist[i] ].second->mutations.size() ; ++mut )
-	{
-	  double mutpos =  diploids[ indlist[i] ].second->mutations[mut]->pos;
-	  itr = find_if(datablock_neut.begin(),
-			datablock_neut.end(),
-			boost::bind(KTfwd::find_mut_pos(),_1,mutpos));
-	  if( itr == datablock_neut.end() )
-	    {
-	      datablock_neut.push_back( make_pair(mutpos,string('0',2*ttl)) );
-	      datablock_neut[datablock_neut.size()-1].second[2*i + 1] = '1';
-	    }
-	  else
-	    {
-	      itr->second[offset + 2*i + 1] = '1';
-	    }
-	}
-      //selected
-      for( unsigned mut = 0 ; mut < diploids[ indlist[i] ].first->smutations.size() ; ++mut )
-	{
-	  double mutpos =  diploids[ indlist[i] ].first->smutations[mut]->pos;
-	  itr = find_if(datablock_sel.begin(),
-			datablock_sel.end(),
-			boost::bind(KTfwd::find_mut_pos(),_1,mutpos));
-	  if( itr == datablock_sel.end() )
-	    {
-	      datablock_sel.push_back( make_pair(mutpos,string('0',ttl)) );
-	      datablock_sel[datablock_sel.size()-1].second[offset + 2*i] = '1';
-	    }
-	  else
-	    {
-	      itr->second[2*i] = '1';
-	    }
-	}
-      for( unsigned mut = 0 ; mut < diploids[ indlist[i] ].second->smutations.size() ; ++mut )
-	{
-	  double mutpos =  diploids[ indlist[i] ].second->smutations[mut]->pos;
-	  itr = find_if(datablock_sel.begin(),
-			datablock_sel.end(),
-			boost::bind(KTfwd::find_mut_pos(),_1,mutpos));
-	  if( itr == datablock_sel.end() )
-	    {
-	      datablock_sel.push_back( make_pair(mutpos,string('0',2*ttl)) );
-	      datablock_sel[datablock_sel.size()-1].second[2*i + 1] = '1';
-	    }
-	  else
-	    {
-	      itr->second[offset + 2*i + 1] = '1';
-	    }
-	}
-    }
-}
-
-cc_intermediate process_population( const vector< pair<glist::iterator,glist::iterator> > & diploids,
-				    const vector<pair<double,double> > & phenotypes,
-				    const vector<size_t> & put_controls,
-				    const vector<size_t> & put_cases,
-				    const unsigned & ncontrols,
-				    const unsigned & ncases)
-{
-  cc_intermediate rv;
-
-  vector< pair<double,string> > neutral,selected;
-  vector< pair<double,string> >::iterator itr;
-  //Go thru controls first
-  process_subset( neutral, selected,
-		  rv.phenotypes,
-		  diploids,
-		  phenotypes,
-		  put_controls,
-		  ncontrols,
-		  2*(ncontrols+ncases),
-		  0 );
-  //cases
-  process_subset( neutral, selected,
-		  rv.phenotypes,
-		  diploids,
-		  phenotypes,
-		  put_cases,
-		  ncases,
-		  2*(ncontrols+ncases),
-		  2*ncontrols);
-
-  sort( neutral.begin(), neutral.end(), boost::bind(KTfwd::sortpos(),_1,_2) );
-  sort( selected.begin(), selected.end(), boost::bind(KTfwd::sortpos(),_1,_2) );
-
-  rv.neutral.assign( neutral.begin(), neutral.end() );  
-  rv.causative.assign( selected.begin(), selected.end() );  
-
-  RemoveInvariantColumns(&rv.neutral);
-  RemoveInvariantColumns(&rv.causative);
-
-  //Define the minor allele state
-  for( SimData::const_site_iterator i = rv.neutral.sbegin() ; 
-       i < rv.neutral.send() ; ++i )
-    {
-      size_t c = count(i->second.begin(),i->second.begin() + 2*ncontrols,'1');
-      rv.min_n.push_back( (c <= ncontrols) ? '1' : '0' );
-    }
-  for( SimData::const_site_iterator i = rv.causative.sbegin() ; 
-       i < rv.neutral.send() ; ++i )
-    {
-      size_t c = count(i->second.begin(),i->second.begin() + 2*ncontrols,'1');
-      rv.min_c.push_back( (c <= ncontrols) ? '1' : '0' );
-    }
-  return rv;
 }
