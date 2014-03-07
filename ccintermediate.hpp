67fbf2e0a75108b9d9a119a04646876b7e7802d9
Kevin R. Thornton <krthornt@uci.edu>
2014-03-07 14:53:09 -0800

    New files

---
+++ ccintermediate.hpp
@@ -0,0 +1,25 @@
+#ifndef __CCINTERMEDIATE_HPP__
+#define __CCINTERMEDIATE_HPP__
+
+#include <vector>
+#include <list>
+#include <functional>
+
+#include <Sequence/SimData.hpp>
+#include <mutation_with_age.hpp>
+
+struct cc_intermediate
+{
+  Sequence::SimData neutral,causative;
+  std::vector<char> min_n,min_c; //minor alleles, defined from controls
+  std::vector<std::pair<double,double> > phenotypes;
+};
+
+cc_intermediate process_population( const std::vector< std::pair<glist::iterator,glist::iterator> > & diploids,
+				    const std::vector<std::pair<double,double> > & phenotypes,
+				    const std::vector<size_t> & put_controls,
+				    const std::vector<size_t> & put_cases,
+				    const unsigned & ncontrols,
+				    const unsigned & ncases);
+
+#endif
