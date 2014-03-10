61dc8e79213af19d4c1d3d4148ad271eb8478599
Kevin R. Thornton <krthornt@uci.edu>
2014-03-10 15:51:26 -0700

    Moving towards boost::program_options...

--- esm_chisq_zscore.cc
+++ esm_chisq_zscore.cc
@@ -16,6 +16,7 @@
 #include <algorithm>
 #include <numeric>
 
+#include <boost/program_options.hpp>
 #include <boost/function.hpp>
 #include <boost/bind.hpp>
 #include <boost/thread.hpp>
@@ -36,28 +37,39 @@ void permute( const CCblock * ccdata,
 	      const unsigned & seed,
 	      vector<double> * rv );
 
+struct params
+{
+  string ccindexfile,ccfile,outfile;
+  unsigned record_no,K,max_controls,max_cases,nperms,nthreads,seed;
+  double minfreq,maxfreq,rsq_cutoff;
+};
+
+params parse_command_line(int argc, char ** argv);
+
 int main(int argc, char ** argv)
 {
-  int argn = 1;
-  const char * ccindexfile = argv[argn++];
-  const char * ccfile = argv[argn++];
-  const unsigned record_no = atoi(argv[argn++]);
-  const double minfreq = atof(argv[argn++]);
-  const double maxfreq = atof(argv[argn++]);
-  const unsigned K = atoi(argv[argn++]);
-  const double rsq_cutoff = atof(argv[argn++]);
-  const unsigned max_controls = atoi(argv[argn++]);
-  const unsigned max_cases = atoi(argv[argn++]);
-  const unsigned nperms = atoi(argv[argn++]);
-  const char * outfile = argv[argn++];
-  const unsigned nthreads = atoi(argv[argn++]);
-  const unsigned seed = atoi(argv[argn++]);
+  // int argn = 1;
+  // //const char * ccindexfile = argv[argn++];
+  // //const char * ccfile = argv[argn++];
+  // const unsigned record_no = atoi(argv[argn++]);
+  // const double minfreq = atof(argv[argn++]);
+  // const double maxfreq = atof(argv[argn++]);
+  // const unsigned K = atoi(argv[argn++]);
+  // const double rsq_cutoff = atof(argv[argn++]);
+  // const unsigned max_controls = atoi(argv[argn++]);
+  // const unsigned max_cases = atoi(argv[argn++]);
+  // const unsigned nperms = atoi(argv[argn++]);
+  // //const char * outfile = argv[argn++];
+  // const unsigned nthreads = atoi(argv[argn++]);
+  // const unsigned seed = atoi(argv[argn++]);
+
+  params options = parse_command_line(argc, argv);
 
   bool fail = true;
-  CCblock ccdata = read_CC_record(ccindexfile,ccfile,record_no,&fail,max_controls,max_cases,true);
+  CCblock ccdata = read_CC_record(options.ccindexfile.c_str(),options.ccfile.c_str(),options.record_no,&fail,options.max_controls,options.max_cases,true);
   if(fail)
     {
-      cerr << "Error: could not find record " << record_no << " in " << ccindexfile << '\n';
+      cerr << "Error: could not find record " << options.record_no << " in " << options.ccindexfile << '\n';
       exit(10);
     }
 
@@ -67,38 +79,37 @@ int main(int argc, char ** argv)
       ccstatus.push_back(1);
     }
   assert(ccstatus.size() == ccdata.geno_matrix[0].size());
-  vector<short> keep = filter_sites(ccdata,minfreq,maxfreq,rsq_cutoff);
+  vector<short> keep = filter_sites(ccdata,options.minfreq,options.maxfreq,options.rsq_cutoff);
 
   vector<double> chisqs = chisq_per_marker( &ccdata, &keep, ccstatus );
 
-  double obs_esm = esm(chisqs,K);
+  double obs_esm = esm(chisqs,options.K);
 
   gsl_rng * r =  gsl_rng_alloc(gsl_rng_taus2);
-  gsl_rng_set(r,seed);
+  gsl_rng_set(r,options.seed);
 
   vector<double> permstats;
-  if(nthreads == 1)
+  if(options.nthreads == 1)
     {
-      permute(&ccdata,K,&keep,nperms,gsl_rng_get(r),&permstats);
+      permute(&ccdata,options.K,&keep,options.nperms,gsl_rng_get(r),&permstats);
     }
-  else if (nthreads > 1)
+  else if (options.nthreads > 1)
     {
-      vector<vector<double> > permstats_thread(nthreads,vector<double>());
-      unsigned nperms_thread = nperms/nthreads;
+      vector<vector<double> > permstats_thread(options.nthreads,vector<double>());
+      unsigned nperms_thread = options.nperms/options.nthreads;
       boost::thread_group tg;
-      for( unsigned i=0;i<nthreads;++i)
+      for( unsigned i=0;i<options.nthreads;++i)
 	{
-	  if( i == 0 && nperms_thread*nthreads < nperms )
+	  if( i == 0 && nperms_thread*options.nthreads < options.nperms )
 	    {
-	      tg.add_thread( new boost::thread( boost::bind(permute,&ccdata,K,&keep,nperms_thread + (nperms-nperms_thread*nthreads),gsl_rng_get(r),&permstats_thread[i]) ) );
+	      tg.add_thread( new boost::thread( boost::bind(permute,&ccdata,options.K,&keep,nperms_thread + (options.nperms-nperms_thread*options.nthreads),gsl_rng_get(r),&permstats_thread[i]) ) );
 	    }
 	  else
 	    {
-	      tg.add_thread( new boost::thread( boost::bind(permute,&ccdata,K,&keep,nperms_thread,gsl_rng_get(r),&permstats_thread[i]) ) );
+	      tg.add_thread( new boost::thread( boost::bind(permute,&ccdata,options.K,&keep,nperms_thread,gsl_rng_get(r),&permstats_thread[i]) ) );
 	    }
 	}
       tg.join_all();
-      
       
       for(unsigned i=0;i<permstats_thread.size();++i)
 	{
@@ -106,34 +117,34 @@ int main(int argc, char ** argv)
 	  copy(permstats_thread[i].begin(),permstats_thread[i].end(),
 	       std::back_inserter(permstats));
 	}
-      assert( permstats.size() == nperms );
+      assert( permstats.size() == options.nperms );
       permstats_thread.clear();
 
     }
   sort(permstats.begin(),permstats.end());
   double perm_p = double( count_if(permstats.begin(),permstats.end(),
-				   boost::bind(greater<double>(),_1,obs_esm ) ) )/double(nperms);
+				   boost::bind(greater<double>(),_1,obs_esm ) ) )/double(options.nperms);
   double mean = gsl_stats_mean(&permstats[0],1,permstats.size());
   double sd = gsl_stats_sd(&permstats[0],1,permstats.size());
   double z = (obs_esm-mean)/sd;
 
-  FILE * fp = fopen(outfile,"a");
+  FILE * fp = fopen(options.outfile.c_str(),"a");
   int fd = fileno(fp);
 
   flock fd_lock = get_whole_flock();
 
   if (fcntl(fd,F_SETLKW,&fd_lock) == -1)
     {
-      cerr << "ERROR: could not obtain lock on " << outfile << '\n';
+      cerr << "ERROR: could not obtain lock on " << options.outfile << '\n';
       exit(10);
     }
 
-  fprintf(fp,"%u\t%lf\t%lf\n",record_no,perm_p,z);
+  fprintf(fp,"%u\t%lf\t%lf\n",options.record_no,perm_p,z);
 
   fd_lock.l_type = F_UNLCK;
   if (fcntl(fd,F_UNLCK,&fd_lock) == -1)
     {
-      cerr << "ERROR: could not release lock on " << outfile << '\n';
+      cerr << "ERROR: could not release lock on " << options.outfile << '\n';
       exit(10);
     }
   fclose(fp);
@@ -161,4 +172,11 @@ void permute( const CCblock * ccdata,
       rv->push_back( esm(chisqs,K) );
     }
   gsl_rng_free(r);
+}
+
+params parse_command_line(int argc, char ** argv)
+{
+  params rv;
+
+  return rv;
 }
