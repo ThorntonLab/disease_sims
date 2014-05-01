c1b78425929f77955d06e7c8344cb0e5b8ee8653
Kevin R. THORNTON <krthornt@hpc-login-1-2.local>
2014-05-01 05:28:53 -0700

    added exit(0) to main()

--- TFL2013_ind.cc
+++ TFL2013_ind.cc
@@ -239,7 +239,7 @@ int main(int argc, char ** argv)
 	  KTfwd::remove_fixed_lost(&mutations,&fixations,&fixation_times,&lookup,ttl_gen,2*params.N);
 	}
     }
-
+  cerr << gametes.size() << '\n';
 
   //Write out the population
   ostringstream popbuffer;
@@ -454,6 +454,8 @@ int main(int argc, char ** argv)
     }
   fflush( index_fh );
   fclose(index_fh);
+
+  exit(0);
 }
 
 simparams parse_command_line(const int & argc,
