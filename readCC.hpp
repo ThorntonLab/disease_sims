144e835e4a407e58abcd6eebb46629e606fdbe36
Kevin R. THORNTON <krthornt@hpc-login-1-2.local>
2014-05-02 10:19:15 -0700

    now reads in number of causative mutations per haplo and the phenotypes

--- readCC.hpp
+++ readCC.hpp
@@ -2,20 +2,25 @@
 #define __READ_CC_HPP__
 
 #include <vector>
+#include <utility>
 #include <limits>
+
 struct CCblock
 {
-
   //SN = no. segregating neutral markers
   //SC = no. segregating causative markers
   unsigned ncontrols,ncases,SN,SC;
   std::vector<double> neutral_pos,caus_pos;
   std::vector<std::vector<unsigned> > geno_matrix;
+  std::vector<std::pair<unsigned,unsigned> > burden;
+  std::vector<std::pair<double,double> > phenos;
   CCblock( const unsigned & nco,
 	   const unsigned & nca,
 	   const std::vector<double> & np,
 	   const std::vector<double> & cp,
-	   const std::vector< std::vector<unsigned> > & gm );
+	   const std::vector< std::vector<unsigned> > & gm,
+	   const std::vector< std::pair<unsigned,unsigned> > & b,
+	   const std::vector< std::pair<double,double> > & ph );
   CCblock(void);
 };
 
