9d82fb3dc12ace88e7963392b3ba39c94a37f4af
Kevin R. THORNTON <krthornt@hpc-login-1-2.local>
2014-05-09 07:47:21 -0700

    New files for reading in sim output

---
+++ common/readSimOutput.hpp
@@ -0,0 +1,24 @@
+#ifndef __READ_SIM_OUTPUT_HPP__
+#define __READ_SIM_OUTPUT_HPP__
+
+#include <vector>
+#include <iostream>
+
+struct recOffsets
+{
+  long pop_offset,pheno_offset,effect_offset;
+  bool found;
+  recOffsets(void);
+};
+
+struct effectFileData
+{
+  //age is in generations9
+  double pos,esize,count,age;
+};
+
+recOffsets read_index( const char * idxfile,
+		       const unsigned & record_no );
+
+std::vector<effectFileData> read_effect_file( std::istream & i );
+#endif
