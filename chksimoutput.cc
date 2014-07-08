86f78dd0b62ec256fb3d039aa2b0c672d5a00b20
Kevin R. THORNTON <krthornt@hpc-login-1-2.local>
2014-07-08 08:48:39 -0700

    better argv parsing using boost

--- chksimoutput.cc
+++ chksimoutput.cc
@@ -2,24 +2,32 @@
 //So far only checks number of mutations
 
 #include <mutation_with_age.hpp>
-//#include <TFL_fitness_models.hpp>
 #include <simindex.hpp>
 #include <fwdpp/IO.hpp>
 #include <zlib.h>
 #include <boost/bind.hpp>
 #include <iostream>
+#include <string>
+#include <boost/program_options.hpp>
+
 using namespace std;
+using namespace boost::program_options;
+
+struct options
+{
+  string ifile,pfile,efile;
+};
+
+options pargs(int argc, char ** argv);
 
 int main( int argc, char ** argv )
 {
   int argn=1;
-  const char * indexfile = argv[argn++];
-  const char * popfile = argv[argn++];
-  const char * effectfile = argv[argn++];
+  options o = pargs(argc,argv);
 
-  simindex idx(indexfile);
+  simindex idx(o.ifile.c_str());
 
-  gzFile gzin=gzopen(popfile,"r"),gzin2=gzopen(effectfile,"r");
+  gzFile gzin=gzopen(o.pfile.c_str(),"r"),gzin2=gzopen(o.efile.c_str(),"r");
 
   for( unsigned i = 1 ; i <= idx.size() ; ++i )
     {
@@ -80,4 +88,29 @@ int main( int argc, char ** argv )
 	       << " checks out ok\n";
 	}
     }
+}
+
+options pargs(int argc, char ** argv)
+{
+  options rv;
+  options_description desc("Reads through the simulation output and index file to validate the output.  Currently, the program reads the index file, the population file, and the effects file, and makes sure that all the expected mutations in the population file and effects file match up.");
+  desc.add_options()
+    ("help,h","Show help")
+    ("index,i",value<string>(&rv.ifile),"Index file for simulation output")
+    ("pop,p",value<string>(&rv.pfile),"Binary file containing population")
+    ("effects,e",value<string>(&rv.efile),"Binary file containing mutation effects")
+    ;
+
+  variables_map vm;
+  store(parse_command_line(argc, argv, desc), vm);
+  notify(vm);
+
+  if(argc == 1 || vm.count("help") ||
+     (!vm.count("index")||!vm.count("pop")||!vm.count("effects")))
+    {
+      cerr << desc << '\n';
+      exit(0);
+    }
+
+  return rv;
 }
