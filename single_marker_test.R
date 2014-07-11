60120d1ef1710148b41ab06209e4fdc090849eaa
Kevin R. THORNTON <krthornt@hpc-login-1-3.local>
2014-07-11 08:04:45 -0700

    adding as.integer() makes diseaseSims::writePVblock workemacs -nw single_marker_test.R

--- single_marker_test.R
+++ single_marker_test.R
@@ -6,7 +6,7 @@ n=commandArgs(trailing=T)
 indexfile=n[1]
 anovaindex=n[2]
 effectfile=n[3]
-recordno=n[4]
+recordno=as.integer(n[4])  #"type cast" needed here, else passing to writePVblock returns undecipherable error :)
 anovafile=n[5]
 ncontrols=as.integer(n[6])
 ncases=as.integer(n[7])
@@ -42,9 +42,12 @@ status=c(rep(0,ccdata$ncontrols),rep(1,ccdata$ncases))
 pvblock=makePVblock(ccdata,esizes,status)
 pvblock[,"popfreq"]=pvblock[,"popfreq"]/(2*N)
 
-options(warn=-1)
-f=pipe(paste(blocker,recordno,outindex,outfile,as.integer(nrow(pvblock)),sep=" "))
-write.table(pvblock,f,row.names=F,col.names=T,quote=F,append=T)
-#writePVblock(outfile,outindex,recordno,pvblock)
+#OLD CODE:
+#options(warn=-1)
+#f=pipe(paste(blocker,recordno,outindex,outfile,as.integer(nrow(pvblock)),sep=" "))
+#write.table(pvblock,f,row.names=F,col.names=T,quote=F,append=T)
+
+#Uses Rcpp to manage POSIX file locking from within R:
+writePVblock(outfile,outindex,recordno,pvblock)
 
 
