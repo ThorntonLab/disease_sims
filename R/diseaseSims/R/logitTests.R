6a4d41526493fb7d054912684e72328458113b9e
Kevin R. THORNTON <krthornt@hpc-login-1-3.local>
2014-07-16 14:39:00 -0700

    now depends on buRden

--- R/diseaseSims/R/logitTests.R
+++ R/diseaseSims/R/logitTests.R
@@ -1,88 +1,3 @@
-#' Association testing under an additive model
-#' @param x An array of genotypes, coded as number of copies of minor allele
-#' @param status An array of 0 = control, 1 = case.  length(status) must equal length(x)
-#' @return The p-value of a logistic regression of case/control status onto genotype.
-logit.additive=function(x,status)
-{
-    if(length(x) != length(status))
-        {
-            stop("logit.additive: length(x) != length(status)")
-            return;
-        }
-    GLM=glm( status ~ x, family=binomial("logit") )
-    return( as.numeric( summary(GLM)$coefficients[,4][2] ) )
-}
-
-#' Association testing under a recessive model
-#' @param x An array of genotypes, coded as number of copies of minor allele
-#' @param status An array of 0 = control, 1 = case.  length(status) must equal length(x)
-#' @note The test works by recoding 1 genotypes as 0 (e.g., one copy of minor allele is the same as none)
-#' @return The p-value of a logistic regression of case/control status onto genotype.
-logit.recessive=function(x,status)
-    {
-        if(length(x) != length(status))
-            {
-                stop("logit.additive: length(x) != length(status)")
-                return;
-            }
-        x[which(x==1)]=0
-        GLM=glm( status ~ x, family=binomial("logit") )
-        return( as.numeric( summary(GLM)$coefficients[,4][2] ) )
-    }
-
-#' Association testing under a dominant model
-#' @param x An array of genotypes, coded as number of copies of minor allele
-#' @param status An array of 0 = control, 1 = case.  length(status) must equal length(x)
-#' @note The test works by recoding 2 genotypes as 1 (e.g., one copy of minor allele is the same as two)
-#' @return The p-value of a logistic regression of case/control status onto genotype.
-logit.dominant=function(x,status)
-    {
-        if(length(x) != length(status))
-            {
-                stop("logit.dominant: length(x) != length(status)")
-                return;
-            }
-        x[which(x==2)]=1
-        GLM=glm( status ~ x, family=binomial("logit") )
-        return( as.numeric( summary(GLM)$coefficients[,4][2] ) )
-    }
-
-#' Obtain single-marker p-values for case/control data
-#' @param genos A matrix of genotypes, coded as number of copies of minor allele
-#' @param status An array of 0 = control, 1 = case.  length(status) must equal nrow(x)
-#' @param model One of "additive","recessive", or "dominant"
-#' @return An array of p-values of logistic regressions of case/control status onto genotype.  The order of the p-values corresponds to the column order in x.
-#' @examples
-#' data(ccblock.dsdata)
-#' #The function works on the genotype matrix
-#' #We assign discrete phenotypes based on number of controls & cases in ccblock.dsdata$genos:
-#' ccblock.dsdata.pvals = ccpvals(ccblock.dsdata$genos,c(rep(0,ccblock.dsdata$ncontrols),rep(1,ccblock.dsdata$ncases)))
-ccpvals = function(genos,status,model="additive")
-    {
-        if( length(status) != nrow(genos) )
-            {
-                stop("ccpvals.additive: nrow(x) != length(status)")
-                return;
-            }
-        if( model == "additive" )
-            {
-                return(apply(genos,2,logit.additive,status))
-            }
-        else if ( model == "recessive" )
-            {
-                return(apply(genos,2,logit.recessive,status))
-            }
-        else if (model == "dominant")
-            {
-                return(apply(genos,2,logit.dominant,status))
-            }
-        else
-            {
-                stop(paste("ccpvals: model",model,"is not valid. Model must be one of additive, dominant, or recessive"))
-            }
-        return();
-    }
-
 #' Perform single-marker association test on simulated case/control data
 #' @param ccdata A case/control data set returned from \link[diseaseSims]{getCCblock}
 #' @param esizes A matrix summarizing mutation effect sizes returned from \link[diseaseSims]{getEsizes}
@@ -117,6 +32,6 @@ makePVblock = function( ccdata,
                 output[r,"esize"]=as.numeric(esizes$esize[z])
                 output[r,"popfreq"]=as.numeric(esizes$count[z])
             }
-        output[,"score"]=-log10(ccpvals(ccdata$genos,status,model))
+        output[,"score"]=-log10(buRden::ccpvals(ccdata$genos,status,model))
         return(as.data.frame(output))
     }
--- R/diseaseSims/redoc_hpc.sh
+++ R/diseaseSims/redoc_hpc.sh
@@ -1,4 +1,4 @@
 #!sh
 
 module load R krthornt/fwdpp/0.2.4
-PKG_LIBS=$LDFLAGS PKG_CPPFLAGS="-I$HOME/src/disease_sims $CPPFLAGS" R_LIBS=/data/users/krthornt/R_libs:$R_LIBS sh redoc.sh
+R_LIBS=~/$HOME/R_libs:$R_LIBS PKG_LIBS=$LDFLAGS PKG_CPPFLAGS="-I$HOME/src/disease_sims $CPPFLAGS" R_LIBS=/data/users/krthornt/R_libs:$R_LIBS sh redoc.sh
