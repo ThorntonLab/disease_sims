2b91afc7f5db08326c43412c7124bdf9962504ff
Kevin R Thornton <krthornt@thornton01.bio.uci.edu>
2015-01-06 13:24:57 -0800

    seed leads to a segfault

--- scripts/test_workflow.sh
+++ scripts/test_workflow.sh
@@ -2,7 +2,7 @@
 
 rm -f simindex.txt pvalues.txt ccindex.txt esm.txt pvalues_index.txt *.bin.gz
 
-SEED=$RANDOM
+SEED=31588
 ../src/TFL2013_ind -1 1000 -g 10000 -i simindex.txt -p popfile.bin.gz -P phenotypes.bin.gz -E effectsizes.bin.gz -n 0.025 -c 0.0025 -r 0.025 -e 0.1 --noise 0.075 -S $RANDOM
 echo $SEED > seedfile
 ../src/make_case_control -i simindex.txt -p popfile.bin.gz -P phenotypes.bin.gz -c ccfile.bin.gz -I ccindex.txt -n 100 -N 100 -t 0.1
