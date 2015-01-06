3eb75be8ef265e5b91aea8129cd00e2e38244790
Kevin R Thornton <krthornt@thornton01.bio.uci.edu>
2015-01-06 10:30:35 -0800

    fixed typos in path

--- scripts/test_workflow.sh
+++ scripts/test_workflow.sh
@@ -2,8 +2,8 @@
 
 rm -f simindex.txt pvalues.txt ccindex.txt esm.txt pvalues_index.txt *.bin
 
-../srcTFL2013_ind -1 1000 -g 10000 -i simindex.txt -p popfile.bin -P phenotypes.bin -E effectsizes.bin -n 0.025 -c 0.0025 -r 0.025 -e 0.1 --noise 0.075 -S $RANDOM
-./src/make_case_control -i simindex.txt -p popfile.bin -P phenotypes.bin -c ccfile.bin -I ccindex.txt -n 100 -N 100 -t 0.1
+../src/TFL2013_ind -1 1000 -g 10000 -i simindex.txt -p popfile.bin -P phenotypes.bin -E effectsizes.bin -n 0.025 -c 0.0025 -r 0.025 -e 0.1 --noise 0.075 -S $RANDOM
+../src/make_case_control -i simindex.txt -p popfile.bin -P phenotypes.bin -c ccfile.bin -I ccindex.txt -n 100 -N 100 -t 0.1
 
 R --no-save --slave --args simindex.txt ccindex.txt popfile.bin 0 ccfile.bin 100 100 1000 pvalues.txt pvalues_index.txt  < ../R/single_marker_test.R
 
