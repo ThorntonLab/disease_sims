b1ed0c353ff3bad0dd17c0a99c5b3c184886953b
Kevin R Thornton <krthornt@thornton01.bio.uci.edu>
2015-01-06 10:32:03 -0800

    added .gz to binary file names

--- scripts/test_workflow.sh
+++ scripts/test_workflow.sh
@@ -1,10 +1,10 @@
 #!sh
 
-rm -f simindex.txt pvalues.txt ccindex.txt esm.txt pvalues_index.txt *.bin
+rm -f simindex.txt pvalues.txt ccindex.txt esm.txt pvalues_index.txt *.bin.gz
 
-../src/TFL2013_ind -1 1000 -g 10000 -i simindex.txt -p popfile.bin -P phenotypes.bin -E effectsizes.bin -n 0.025 -c 0.0025 -r 0.025 -e 0.1 --noise 0.075 -S $RANDOM
-../src/make_case_control -i simindex.txt -p popfile.bin -P phenotypes.bin -c ccfile.bin -I ccindex.txt -n 100 -N 100 -t 0.1
+../src/TFL2013_ind -1 1000 -g 10000 -i simindex.txt -p popfile.bin.gz -P phenotypes.bin.gz -E effectsizes.bin.gz -n 0.025 -c 0.0025 -r 0.025 -e 0.1 --noise 0.075 -S $RANDOM
+../src/make_case_control -i simindex.txt -p popfile.bin.gz -P phenotypes.bin.gz -c ccfile.bin.gz -I ccindex.txt -n 100 -N 100 -t 0.1
 
-R --no-save --slave --args simindex.txt ccindex.txt popfile.bin 0 ccfile.bin 100 100 1000 pvalues.txt pvalues_index.txt  < ../R/single_marker_test.R
+R --no-save --slave --args simindex.txt ccindex.txt popfile.bin.gz 0 ccfile.bin.gz 100 100 1000 pvalues.txt pvalues_index.txt  < ../R/single_marker_test.R
 
-#./esm_chisq_zscore -c ccindex.txt -C ccfile.bin -o esm.txt -r 0 -s 101 
+#./esm_chisq_zscore -c ccindex.txt -C ccfile.bin.gz -o esm.txt -r 0 -s 101 
