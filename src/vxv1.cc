fcc8c9afc2005acc5c38b7c0c5cc49a0f30963bc
molpopgen <krthornt@uci.edu>
2015-03-11 14:52:31 -0700

    program gutted.  starting over.

--- src/vxv1.cc
+++ src/vxv1.cc
@@ -15,97 +15,77 @@
 #include <boost/accumulators/statistics/stats.hpp>
 #include <boost/accumulators/statistics/mean.hpp>
 #include <boost/accumulators/statistics/variance.hpp>
+#include <boost/program_options.hpp>
 #include <gsl/gsl_randist.h>
+
+#include <TFL_fitness_models.hpp>
 
 using namespace std;
 using namespace boost::accumulators;
 
-using mean_acc = accumulator_set<double, stats<tag::mean> >;
-
-double getVG( gzFile gzpin ); //not used...
-
-struct fdata 
+struct vxv1params
 {
-  unsigned nm; //number of mutations at this frequency
-  mean_acc ez,ezsq;
-  fdata() : nm(0u),ez(mean_acc()),ezsq(mean_acc()) {}
+  MODEL m;
+  double h;
+  std::string popfile;
+  unsigned nreps;
 };
+
+vxv1params parse_argv( int argc, char ** argv );
+  
+using mean_acc = accumulator_set<double, stats<tag::mean> >;
 
 int main( int argc, char ** argv)
 {
-  int argn=1;
-  const char * popfilename = argv[argn++];
-  const unsigned nreps = atoi(argv[argn++]);
-
-  gzFile gzin = gzopen(popfilename,"rb");
-
-  //map<unsigned, pair<unsigned,mean_acc> > data;
-  map<unsigned,fdata> data;
-  unsigned nrisk = 0;
-  unsigned popsize= 0;
-  for( unsigned i = 0 ; i < nreps ; ++i )
-    {
-      mlist mutations;
-      glist gametes;
-      dipvector diploids;
-      KTfwd::read_binary_pop( &gametes, &mutations, &diploids, std::bind(gzmreader(),std::placeholders::_1),gzin );
-      popsize = diploids.size();
-      for( auto mitr = mutations.begin() ; mitr != mutations.end() ; ++mitr )
-	{
-	  if( ! mitr->neutral )
-	    {
-	      ++nrisk;
-	      auto __i = data.find( mitr->n );
-	      if(__i == data.end())
-		{
-		  data[mitr->n] = fdata();
-		  data[mitr->n].nm++;
-		  data[mitr->n].ez(mitr->s);
-		  data[mitr->n].ezsq(pow(mitr->s,2.));
-		}
-	      else
-		{
-		  __i->second.nm++;
-		  __i->second.ez(mitr->s);
-		  __i->second.ezsq( pow(mitr->s,2.) );
-		}
-	    }
-	}
-    }
-  double SUM = 0.,SUM2=0.;
-  for( const auto & d : data )
-    {
-      double x = double(d.first)/(2.*double(popsize));
-      double ezsq_x = mean(d.second.ezsq);
-      double fx = double(d.second.nm)/double(nrisk);
-      SUM += 0.5*ezsq_x*fx*x*(1.-x);
-
-      double ez_x = mean(d.second.ez);
-      double __y = 0;
-      for( const auto & d2 : data ) {
-	double y = double(d2.first)/(2.*double(popsize));
-	double ez_y = mean(d2.second.ez);
-	double fy = double(d2.second.nm)/double(nrisk);
-	__y += (ez_x*ez_y)*fy*y*(1.-y);
-      }
-      SUM2 += 0.5 * fx*x*(1.-x)*__y;
-      //}
-      //SUM2 += 0.5*(double(d.second.first)/double(nrisk))*x2;
-      cout << x << ' ' << SUM << ' ' << SUM2 << '\n';
-      //cout << p << ' ' << SUM << ' ' << SUM2 << '\n';
-    }
+  const vxv1params pars = parse_argv(argc, argv);
 }
 
-double getVG( gzFile gzpin )
+vxv1params parse_argv( int argc, char ** argv )
 {
-  unsigned nvals;
-  gzread(gzpin,&nvals,sizeof(unsigned));
-  double val;
-  accumulator_set<double, stats<tag::variance> > av;
-  for( unsigned i = 0 ; i < 2*nvals ; ++i )
+  using namespace boost::program_options;
+  
+  vxv1params rv;
+  rv.m = MODEL::GENE_RECESSIVE;
+  
+  options_description desc("Calculate stuff");
+  desc.add_options()
+    ("help,h","Print usage information to screen")
+    ("popfile,p",value<string>(&rv.popfile),"Name of file containing simulation output")
+    ("nreps,n",value<unsigned>(&rv.nreps)->default_value(0u),"Number of replicates stored in popfile")
+    ("additive","Assume additive genetic model.  (Default is TFL2013)")
+    ("multiplicative","Assume multiplicative genetic model.  (Default is TFL2013)")
+    ("popgen","Assume multiplicative genetic model with dominance.  (Default is TFL2013)")
+    ("dominance",value<double>(&rv.h)->default_value(0.0),"Dominance for multiplicative model with dominance")
+    ("eyrewalker","Use Eyre-Walker 2010 model (not implemented yet!)")
+    ;
+
+  variables_map vm;
+  store(parse_command_line(argc, argv, desc), vm);
+  notify(vm);
+  
+  if(argc == 1 || vm.count("help"))
     {
-      gzread(gzpin,&val,sizeof(double));
-      if( i%2==0. ) av(val);
+      cerr << desc << '\n';
+      exit(0);
     }
-  return variance(av);
+
+  if(vm.count("additive"))
+    {
+      rv.m = MODEL::GENE_ADDITIVE;
+    }
+  else if(vm.count("multiplicative"))
+    {
+      rv.m = MODEL::MULTIPLICATIVE;
+    }
+  else if(vm.count("popgen"))
+    {
+      rv.m = MODEL::POPGEN;
+    }
+
+  if(vm.count("eyrewalker"))
+    {
+      cerr << "Eyre-Walker 2010 not implemented yet!\n";
+      exit(EXIT_SUCCESS);
+    }
+  return rv;
 }
