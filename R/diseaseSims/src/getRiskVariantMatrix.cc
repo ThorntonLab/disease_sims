2f8220bf385f08af1500d78879d58f5ab7c7b7e5
Kevin R. Thornton <krthornt@uci.edu>
2015-03-18 11:13:08 -0700

    new file

---
+++ R/diseaseSims/src/getRiskVariantMatrix.cc
@@ -0,0 +1,12 @@
+#include <Rcpp.h>
+#include <diseaseSims/traitValues.hpp>
+#include <string>
+
+
+Rcpp::List getRiskVariantMatrix_details( const std::string & model,
+					 const std::string & popfile,
+					 const  z_off_t & popfile_offset,
+					 const unsigned & record_id_no)
+{
+}
+
