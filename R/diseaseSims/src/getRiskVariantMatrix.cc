5ce963eee14d0cb10c2638bb5359cf37a13a927b
Kevin R. Thornton <krthornt@uci.edu>
2015-03-19 10:03:26 -0700

    allow for handling of phenotype data

--- R/diseaseSims/src/getRiskVariantMatrix.cc
+++ R/diseaseSims/src/getRiskVariantMatrix.cc
@@ -42,6 +42,24 @@ vector<pair<mlist::iterator,unsigned> > getRiskIndexes( mlist & mutations )
   return risk_indexes;
 }
 
+Rcpp::IntegerMatrix MakeRiskMatrik( const dipvector & diploids,
+				    const vector<pair<mlist::iterator,unsigned> > & risk_indexes )
+{
+  Rcpp::IntegerMatrix genos(diploids.size(),risk_indexes.size());
+  for( unsigned ind = 0 ; ind < diploids.size() ; ++ind )
+    {
+      vmcount_t vmc = get_mut_counts(diploids[ind].first,diploids[ind].second);
+      for( unsigned i = 0 ; i < vmc.size() ; ++i )
+	{
+	  auto __itr = find_if( risk_indexes.begin(), risk_indexes.end(),[&vmc,&i](const pair<mlist::iterator,unsigned> & __p) {
+	      return __p.first == vmc[i].first;
+	    });
+	  genos(ind,__itr->second) += vmc[i].second;
+	}
+    }
+  return genos;
+}
+
 // Details of how to get a genotype matrix for risk variants
 //[[Rcpp::export(".getRiskVariantMatrixDetails")]]
 Rcpp::List getRiskVariantMatrixDetails( const std::string & model,
@@ -68,20 +86,55 @@ Rcpp::List getRiskVariantMatrixDetails( const std::string & model,
   vector<pair<mlist::iterator,unsigned> > risk_indexes = getRiskIndexes(pop.mutations);
   auto Gvals = getG(pop.diploids,dipG);
 
-  Rcpp::IntegerMatrix genos(pop.diploids.size(),risk_indexes.size());
+  Rcpp::IntegerMatrix genos = MakeRiskMatrik(pop.diploids,risk_indexes);
 
-  for( unsigned ind = 0 ; ind < pop.diploids.size() ; ++ind )
-    {
-      vmcount_t vmc = get_mut_counts(pop.diploids[ind].first,pop.diploids[ind].second);
-      for( unsigned i = 0 ; i < vmc.size() ; ++i )
-	{
-	  auto __itr = find_if( risk_indexes.begin(), risk_indexes.end(),[&vmc,&i](const pair<mlist::iterator,unsigned> & __p) {
-	      return __p.first == vmc[i].first;
-	    });
-	  genos(ind,__itr->second) += vmc[i].second;
-	}
-    }
   return Rcpp::List::create(Rcpp::Named("G") = Gvals,
+			    Rcpp::Named("genos") = genos);
+ }
+				    
+// Details of how to get a genotype matrix for risk variants
+//[[Rcpp::export(".getRiskVariantMatrixDetailsPheno")]]
+Rcpp::List getRiskVariantMatrixDetails_Pheno( const std::string & model,
+					      const std::string & popfile,
+					      const int64_t & popfile_offset,
+					      const std::string & phenofile,
+					      const int64_t & phenofile_offset,
+					      const unsigned & record_id_no,
+					      const double & dominance = 0.)
+{
+  gzFile gzin = gzopen(popfile.c_str(),"rb");
+  if( gzin == NULL ) 
+    {
+      Rcpp::Rcerr << "Error, " << popfile
+		  << " could not be opened for reading.\n";
+      return Rcpp::List();
+    }
+
+  gzseek(gzin,popfile_offset, SEEK_SET);
+
+  Gfxn_t dipG = setModel(model,dominance);
+
+  popstruct pop = readPop(gzin);
+  gzclose(gzin);
+
+  gzin = gzopen(phenofile.c_str(),"rb");
+  gzseek(gzin,phenofile_offset,SEEK_SET);
+  unsigned ndips = 0;
+  gzread(gzin,&ndips,sizeof(unsigned));
+  vector<double> phenotypes(ndips);
+  for( unsigned i = 0 ; i < ndips ; ++i )
+    {
+      double G,E;
+      gzread(gzin,&G,sizeof(double));
+      gzread(gzin,&E,sizeof(double));
+      phenotypes[i] = G + E;
+    }
+  gzclose(gzin);
+  vector<pair<mlist::iterator,unsigned> > risk_indexes = getRiskIndexes(pop.mutations);
+
+  Rcpp::IntegerMatrix genos = MakeRiskMatrik(pop.diploids,risk_indexes);
+
+  return Rcpp::List::create(Rcpp::Named("trait") = phenotypes,
 			    Rcpp::Named("genos") = genos);
  }
 
