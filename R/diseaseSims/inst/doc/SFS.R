716ed99012b0c7e087c4ee4e2db1e8d0e14f7649
molpopgen <krthornt@uci.edu>
2015-03-26 17:33:11 -0700

    SFS vignette now based on real example

--- R/diseaseSims/inst/doc/SFS.R
+++ R/diseaseSims/inst/doc/SFS.R
@@ -1,1 +1,23 @@
+## ----, fig.height=5, fig.width=5-----------------------------------------
+library(diseaseSims)
+library(dplyr)
+##n = 100, and if you use n >= 2N, 2N will be used
+##seed = 101
+sfs.all = sfs(system.file("extdata", "pop.bin.gz", package = "diseaseSims"),100,101)
+
+##Normalize the SFS
+sfs.norm = sfs.all %>%
+    group_by(replicate) %>%
+        mutate( n.norm = n/sum(n),
+                r.norm = r/sum(r) )
+
+##Average, normalized SFS
+sfs.norm.mean = sfs.norm %>%
+    group_by(i) %>%
+        summarise(n.norm.mean = mean(n.norm),
+                  r.norm.mean = mean(r.norm))
+
+plot(sfs.norm.mean$i,sfs.norm.mean$n.norm.mean,xlab=("Frequency (out of 100)"),
+								ylab="Proportion of mutations",ylim=c(0,0.5))
+points(sfs.norm.mean$i,sfs.norm.mean$r.norm.mean,pch=18,col="red")
 
