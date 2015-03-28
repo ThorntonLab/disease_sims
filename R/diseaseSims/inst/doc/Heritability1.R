a3f6272939ed4add96245687a3b531921b28232f
molpopgen <krthornt@uci.edu>
2015-03-28 09:27:00 -0700

    updated heritability vignette

--- R/diseaseSims/inst/doc/Heritability1.R
+++ R/diseaseSims/inst/doc/Heritability1.R
@@ -16,3 +16,22 @@ for(i in 1:nrow(idx))
 print(mean(h2))
 print(h2)
 
+## ------------------------------------------------------------------------
+library(diseaseSims)
+idx=read.table(system.file("extdata", "index.txt", package = "diseaseSims"))
+pfile=system.file("extdata", "phenotypes.bin.gz", package = "diseaseSims")
+
+h2=array()
+OFFSET=0
+for( i in 1:nrow(idx) )
+{
+	m = getPheno(pfile,OFFSET)
+	h2[i]=var(m[,1])/( var(m[,1]) + var(m[,2]) )
+	##Update the OFFSET
+	OFFSET = OFFSET + idx$V3[i]
+}
+##Now, you can get the mean, etc., which will not be very accurate for this example,
+##as it is based on just 3 replicates
+print(mean(h2))
+print(h2)
+
