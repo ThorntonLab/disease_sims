b1c060390919ee66fe5d2ac8e93a36bfbd923538
Kevin R. Thornton <krthornt@uci.edu>
2015-04-09 11:42:47 -0700

    correct documentation

--- R/diseaseSims/R/getRiskVariantMatrix.R
+++ R/diseaseSims/R/getRiskVariantMatrix.R
@@ -5,8 +5,10 @@
 #' @param phenofileOffset The offset (in bytes) of the relevant record in phenofilename (optional)
 #' @param modelName The model of gene action.  Must be one of: recessive, additive, multiplicative, or popgen
 #' @param dominance The dominance of a risk mutation.  Only applies to the "popgen" model of gene action
-#' @return traits, which is a list of trait (or genetic) values for each individual
-#' @return genos, which is an data frame of genotypes. Rows = individuals. Columns = 0,1,2 copies of risk mutation
+#' @return genos, which is an data frame of genotypes. Rows = individuals. The first column contains trait
+#' values for each individual, and the remaining columns = 0,1,2 copies of risk mutation
+#' @return esizes, which is a numeric vector of effect sizes for each mutation in genos
+#' @return positions, which are the positions of every matrix in genos
 #' @details
 #' The order of the columns in "genos" is in descending order of both frequency and absolute value of effect size
 #' If phenofilename and phenofileOffset are both provided, then "traits" corresponds to individual phenotypes.
